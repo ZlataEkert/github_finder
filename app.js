70a64c1ab44ba7b97f306d9cddfdf3e8da1bdfb7
ZlataEkert <zlataekert@gmail.com>
2022-10-11 13:59:48 +0300

    fetch profile data

---
+++ app.js
@@ -0,0 +1,25 @@
+// Init GitHub
+const github = new Github;
+
+// Search Input
+const searchUser = document.getElementById('searchUser');
+
+// Search Input Event Listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get Input Text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+    // Make http call
+    github.getUser(userText)
+      .then(data => {
+        if(data.profile.message === 'Not Found') {
+          // Show Alert
+        } else {
+          // Show profile
+        }
+      })
+  } else {
+    // Clear profile
+  }
+});
